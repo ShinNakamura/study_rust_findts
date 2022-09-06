04698a73d2c1726235c68e4988da11f650c35f6c
Shin Nakamura <kokoronopython@gmail.com>
2022-09-06 19:27:39 +0900

    + is_dir

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ type MyResult = Result<(), Box<dyn std::error::Error>>;
 fn main() -> MyResult {
     let args: Vec<String> = env::args().collect();
     let ptrn = &args[1]; // e.g. "**/*txt"
-    println!("modified,filepath"); // 簡易的にCSV形式で出力
+    println!("is_dir,modified,filepath"); // 簡易的にCSV形式で出力
     for entry in glob(ptrn)? {
         let entry = entry?;
         let metadata = fs::metadata(&entry)?;
@@ -19,7 +19,8 @@ fn main() -> MyResult {
         } else {
             "".to_string()
         };
-        println!(r#"{},"{}""#, time, entry.display());
+        let is_dir = if metadata.is_dir() { 1 } else { 0 };
+        println!(r#"{},{},"{}""#, is_dir, time, entry.display());
     }
 
     Ok(())
